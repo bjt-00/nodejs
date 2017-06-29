d0d73775cfb923cada60a3cf3713ab542211199a
Abdul Kareem <Abdul Kareem>
2017-06-28 21:02:17 -0500

    request url access implementation

--- nodejs/Main.js
+++ nodejs/Main.js
@@ -2,11 +2,17 @@
  * 
  */
 var http = require('http');//include a module
+var url = require('url');
 var myFirstModule = require('./MyFirstModule.js');
 
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write('Welcome <br>');
+    
+    res.write("req.url = "+req.url+"<br>");
+    var q = url.parse(req.url, true).query;
+    var txt = q.p1 + " " + q.p2;
+    res.write("var q = url.parse(req.url, true).query = "+txt+"<br>");
+    
     res.end(myFirstModule.message('bitguiders'));
     //res.end();
 }).listen(8080); 
